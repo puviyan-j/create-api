7ab58794310e50c564188131d445933de51c5cb2
puviyan-j <puviyanj49@gmail.com>
2026-06-16 13:56:47 +0530

    path changed

--- src/generators/copynest.generator.ts
+++ src/generators/copynest.generator.ts
@@ -1,10 +1,18 @@
-import fs from "fs-extra";
-import path from "path"
+
 import type { nestProject } from "../types/answers.js";
-import { execa } from "execa";
+import fs from "fs-extra";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export async function copynestfolder(answer:nestProject ){
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    await fs.copy(path.resolve("./templates/nest"),`./${answer.projectName}`)
+// go to project root (dist -> root)
+const rootDir = path.resolve(__dirname, "../../../");
 
+export async function copynestfolder(answer:nestProject) {
+  const templatePath = path.join(rootDir, "templates", "nest");
+  const targetPath = path.join(process.cwd(), answer.projectName);
+
+  await fs.copy(templatePath, targetPath);
 }
--- src/generators/eslint.generator.ts
+++ src/generators/eslint.generator.ts
@@ -4,19 +4,28 @@ import type { Answers } from "../types/answers.js";
 import fs from "fs-extra";
 import path from "path";
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// go to project root (dist -> root)
+const rootDir = path.resolve(__dirname, "../../../");
+
 export async function generatorEslint(answer: Answers, pathname: string) {
+
     if (answer.language === "typescript") {
-        await fs.copyFile(path.resolve("./templates/eslint/modulets.ts"), path.join(pathname, './eslint.config.ts'));
+        await fs.copyFile(path.join(rootDir,"./templates/eslint/modulets.ts"), path.join(pathname, './eslint.config.ts'));
         return;
     }
 
     if (answer.module === "modulejs") {
-        await fs.copyFile(path.resolve("./templates/eslint/modulejs.ts"), path.join(pathname, './eslint.config.js'));
+        await fs.copyFile(path.join(rootDir,"./templates/eslint/modulejs.ts"), path.join(pathname, './eslint.config.js'));
         return;
     };
 
     if (answer.module === "commonjs") {
-        await fs.copyFile(path.resolve("./templates/eslint/commonjs.ts"), path.join(pathname, './eslint.config.mjs'));
+        await fs.copyFile(path.join(rootDir,"./templates/eslint/commonjs.ts"), path.join(pathname, './eslint.config.mjs'));
         return;
     }
 
